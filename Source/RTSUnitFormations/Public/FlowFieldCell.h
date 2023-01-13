92359e83313d4139c6979bfffff0f8f73ba98eb8
Daphné-Marie Huyghe <Daphnemarie.huyghe@gmail.com>
2023-01-13 15:58:43 +0100

    fix bad include

--- Source/RTSUnitFormations/Public/FlowFieldCell.h
+++ Source/RTSUnitFormations/Public/FlowFieldCell.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "FlowFieldDirection.h"
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "GameFramework/Actor.h"
